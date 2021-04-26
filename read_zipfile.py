293299e95af1a196fe2bb6b1d51e9f476eb7f5b7
Santiago Toso <39338250+Bondify@users.noreply.github.com>
2021-04-26 10:01:14 -0400

    Update read_zipfile.py

--- read_zipfile.py
+++ read_zipfile.py
@@ -14,8 +14,8 @@ import itertools
 import base64
 
 st.set_page_config(layout="wide")
-st.sidebar.header('Upload routes.txt, trips.txt, stop_times.txt and shapes.txt')
-uploaded_files = st.sidebar.file_uploader('File uploader', accept_multiple_files=True, type=['txt'])
+st.sidebar.header('Drag and drop files here')
+uploaded_files = st.sidebar.file_uploader('Upload routes.txt, trips.txt, stop_times.txt and shapes.txt', accept_multiple_files=True, type=['txt'])
 
 # Get the polygons
 polys = gpd.read_file("https://raw.githubusercontent.com/Bondify/km_per_polygon/main/data/polygons.geojson")
