d92420dd03753032a2529968f900f3717570c86b
Dominika Schreyer <132690825+Dominikasc@users.noreply.github.com>
2023-06-20 17:19:15 +0200

    Update read_zipfile.py
    comment out map

--- read_zipfile.py
+++ read_zipfile.py
@@ -309,8 +309,8 @@ if uploaded_files != []:
         
         st.dataframe(table_poly, 900, 600)
         st.markdown(get_table_download_link(table_poly), unsafe_allow_html=True)
-        
-    with col3: 
+      
+   """with col3: 
         # CREATE THE MAP
         st.subheader('Map')
         st.pydeck_chart(pdk.Deck(
@@ -371,4 +371,5 @@ if uploaded_files != []:
                     )
                 ]
         ))
+        """
         
