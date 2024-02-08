c6a2a0b7c717e04942b3d3b1737d2fc2a1da1ea6
Dominika Schreyer <dominika.schreyer@live.de>
2024-02-08 13:19:02 +0100

    added pattern logic from remix

--- read_zipfile_en.py
+++ read_zipfile_en.py
@@ -175,7 +175,8 @@ if uploaded_files != []:
 
     # Get rid of route_id in trip_id
     min_per_shape['trip_id'] = min_per_shape.apply(lambda row: re.sub(r"[\([{})\]]", "", row.trip_id) , axis =1)
-    min_per_shape['trip_id'] = min_per_shape.apply(lambda row: re.sub(row.route_id + r'\s*','', row.trip_id), axis =1)
+    min_per_shape['route_id'] = min_per_shape.apply(lambda row: re.sub(r"[\([{})\]]", "", row.route_id) , axis =1)
+    min_per_shape['trip_id'] = min_per_shape.apply(lambda row: re.sub(row.route_id,'', row.trip_id), axis =1)
 
     # Get split location for pattern
     def splitloc(tripid):
