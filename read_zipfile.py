0969b7af7109d8f3b047b515e5904cc5d9508057
Dominika Schreyer <dominika.schreyer@live.de>
2024-02-12 17:01:00 +0100

    merged long and short name

--- read_zipfile.py
+++ read_zipfile.py
@@ -53,8 +53,7 @@ if uploaded_files != []:
         # Parse the files of the GTFS I need
         if name=='routes.txt':
             routes = pd.read_csv(file)
-            if len(routes.route_short_name.unique()) == 1:
-                routes['route_short_name'] = routes['route_long_name']
+            routes['route_short_name'] = routes['route_short_name'].astype(str) + " " + routes['route_long_name'].astype(str)
                 
         elif name == 'trips.txt':
             trips = pd.read_csv(file)
@@ -147,7 +146,7 @@ if uploaded_files != []:
     stop_times['trip_id'] = stop_times.apply(lambda row: re.sub(r"[\([{})\]]", "", row.trip_id) , axis =1)
     stop_times['route_id'] = stop_times.apply(lambda row: re.sub(r"[\([{})\]]", "", row.route_id) , axis =1)
     stop_times['trip_id'] = stop_times.apply(lambda row: re.sub(row.route_id,'', row.trip_id), axis =1)
-    
+
     # I need the start and end coordinate in shapes
 
     def startcoord(row):
