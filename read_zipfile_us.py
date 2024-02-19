46a035bc32f92da6acbfd8da75c568bc866f728a
Dominika Schreyer <dominika.schreyer@live.de>
2024-02-19 13:04:19 +0100

    remove trips if area not selected

--- read_zipfile_us.py
+++ read_zipfile_us.py
@@ -469,7 +469,11 @@ if uploaded_files != []:
             href = f'<a href="data:file/csv;base64,{b64}">Export CSV file</a>'
             return href
         
-        gb = GridOptionsBuilder.from_dataframe(table_poly) #NEW
+        table_poly_view = table_poly
+        if filter_polys != []:
+            table_poly_view = table_poly.loc[:, table_poly.columns != table_poly['Trips per year']]
+
+        gb = GridOptionsBuilder.from_dataframe(table_poly_view) #NEW
 
         gb.configure_default_column(
             resizable=True,
@@ -522,9 +526,9 @@ if uploaded_files != []:
 
         go = gb.build() #NEW
 
-        AgGrid(table_poly, gridOptions=go, theme="streamlit") #NEW
+        AgGrid(table_poly_view, gridOptions=go, theme="streamlit") #NEW
         #st.dataframe(table_poly, 1200, 600) #NEW
-        st.markdown(get_table_download_link(table_poly), unsafe_allow_html=True)
+        st.markdown(get_table_download_link(table_poly_view), unsafe_allow_html=True)
 
     with col3: 
         # CREATE THE MAP
