eb3592d9a4550155580bafe8082239c59e3de9f3
Dominika Schreyer <dominika.schreyer@live.de>
2024-08-30 12:52:49 +0200

    updated splitloc function for pattern allocation

--- read_zipfile_en.py
+++ read_zipfile_en.py
@@ -109,8 +109,13 @@ if uploaded_files != []:
     def splitloc(tripid):
         loc = 2
         if tripid.startswith('Service'):
-            loc = 3
-        return loc 
+            mylist = re.split('-', tripid)
+
+            try:
+                loc = mylist.index('IB')-1
+            except ValueError:
+                loc = mylist.index('OB')-1
+        return loc
     
     # Add the number of days per year 
 
